0bdcf3333314851ef3e4179066b4b8ffd46b4ae7
Kan Yu <WalterYeYint@gmail.com>
2021-03-21 11:09:16 +0630

    Created input file for Allocation.py. Modified some lines in Allocation.py.

--- Kickstart_Practice/2020/Allocation.py
+++ Kickstart_Practice/2020/Allocation.py
@@ -1,7 +1,14 @@
 T = int(input())
+
 for x in range(1, T + 1):
-    print(x)
-    N = int(input())
-    B = int(input())
-    print("N is {}", format(N))
-    print("B is {}", format(B))
+    # print(x)
+    # [N, B] = int(input().split())
+    N, B = map(int, input().split())
+    print("N is {} \nB is {}".format(N, B))
+    A = input().split()
+#     if len(A) != N:
+#         print("Please enter the no. of characters equal\
+#  to the value N.")
+#     else:
+#         print("A is {}".format(A))
+    
